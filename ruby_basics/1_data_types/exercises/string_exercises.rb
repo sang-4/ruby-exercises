0ede61a9e42f70035d3258bf5b8af3698eeb1cb9
Allan <kipropsallan@gmail.com>
2022-11-07 12:55:02 +0300

    Completed assignment

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -10,40 +10,40 @@ end
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
-  "word"[0..4]
+  word[0, 4]
 end
 
 def capitalize(word)
   # capitalize the first letter of the word
-  "word".capitalize
+  word.capitalize
 end
 
 def uppercase(string)
   # uppercase all letters in the string
-  "Classic".upcase
+  string.upcase
 end
 
 def downcase(string)
   # downcase all letters in the string
-  "Classic".downcase
+  string.downcase
 end
 
 def empty_string(string)
   # return true if the string is empty
-  "".empty?
+  string.empty?
 end
 
 def string_length(string)
   # return the length of the string
-  "Classic".length
+  string.length
 end
 
 def reverse(string)
   # return the same string, with all of its characters reversed
-  "Classic".reverse
+  string.reverse
 end
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
-  "Classic".gsub(" ", "")
+  string.gsub(' ', '')
 end
